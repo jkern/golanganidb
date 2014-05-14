0b9335f19b94d2f99a1261f118716c48a5222da2
chet1211221 <chet1211221@gmail.com>
2014-05-13 19:54:24 -0500

    added creating inital config file

---
+++ setupenv/createconfig.go
@@ -0,0 +1,35 @@
+package setupenv
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func CreateConfig(configfile string, initialconfig *Config) {
+	configfileexists, _ := exists(configfile)
+	if configfileexists == true {
+		fmt.Println("Config file already exists")
+	} else {
+		configfilecreated, err := os.Create(configfile)
+		if err != nil {
+			fmt.Println(err)
+			panic(fmt.Sprintf("%v\n", "Unable to create configuration file for program.  See above line"))
+		}
+		defer configfilecreated.Close()
+		configfilecreated.WriteString("client=" + initialconfig.Client + "\r\n")
+		configfilecreated.WriteString("clientver=" + strconv.Itoa(initialconfig.Clientver) + "\r\n")
+		configfilecreated.WriteString("protover=" + strconv.Itoa(initialconfig.Protover) + "\r\n")
+		configfilecreated.WriteString("url=" + initialconfig.Url + "\r\n")
+		configfilecreated.WriteString("port=" + strconv.Itoa(initialconfig.Port) + "\r\n")
+	}
+
+}
+
+type Config struct {
+	Client    string
+	Clientver int
+	Protover  int
+	Url       string
+	Port      int
+}
