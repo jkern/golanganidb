37baca95c6c5690a503e5118ee931bc464f44fe8
Joseph Kern <timetrap@gmail.com>
2014-05-27 03:01:48 +0430

    Move initialconfig and env.CreateConfig
    Move lines 13 and 14 from main.go into createconfig.go
    
    initialconfig := env.Config{"golanganidb", 1, 1, "http://api.anidb.net", 9001}
    env.CreateConfig(programconfigpath+"/golanganidb.conf", &initialconfig)

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ func main() {
 	programdatapath, programconfigpath := env.CreateDir(env.GetHomeDir())
 	fmt.Println(programdatapath)
 	//fmt.Println(programconfigpath)
+	// lines 13 and 14: these need to be moved from main.go -- jak
 	initialconfig := env.Config{"golanganidb", 1, 1, "http://api.anidb.net", 9001}
 	env.CreateConfig(programconfigpath+"/golanganidb.conf", &initialconfig)
 	//fmt.Printf("%v\n", initialconfig)
